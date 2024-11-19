1693af55353be65eaed2442cbc7dcaf5f1aff32e
apolline-dmg <apmi62328@eleve.isep.fr>
2024-11-19 17:09:46 +0100

    ajout de la page mes candidatures

---
+++ views/mescandidatures.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mes candidatures</title>
+    <link rel="stylesheet" type="text/css" href="mescandidatures.css">
+</head>
+<body>
+    <h1 class="titre">Mes candidatures</h1>
+    <div class="container">
+        <div class="premier_carre">
+            <h3 class="sous-titre">Candidatures reçues</h3>
+            <div class="sous-carre">
+                <div class="profil1">
+                    <span class="initiales">EA</span>
+                </div>
+                <div class="details">
+                    <h4>Promenade 15/11/24</h4>
+                    <h6>Elodie A.</h6>
+                </div>
+            </div>
+            <div class="sous-carre">
+                <div class="profil2">
+                    <span class="initiales">LP</span>
+                 </div>
+                <div class="details">
+                    <h4>Promenade 09/11/24</h4>
+                    <h6>Luc P.</h6>
+                </div>
+            </div>
+            <div class="sous-carre">
+                <div class="profil3">
+                    <span class="initiales">KM</span>
+                </div>
+                <div class="details">
+                    <h4>Promenade 17/11/24</h4>
+                    <h6>Karine M.</h6>
+                </div>
+            </div>
+        </div>
+        <div class="deuxieme_carre">
+            <h3 class="sous-titre">Candidatures envoyées</h3>
+            <div class="sous-carre">
+                <div class="profil4">
+                    <span class="initiales">IA</span>
+                </div>
+                <div class="details">
+                    <h4>Promenade 15/11/24</h4>
+                    <h6>Inès A.</h6>
+                </div>
+            </div>
+            <div class="sous-carre">
+                <div class="profil5">
+                    <span class="initiales">ER</span>
+                </div>
+                <div class="details">
+                    <h4>Promenade 09/11/24</h4>
+                    <h6>Eva R.</h6>
+                </div>
+            </div>
+        </div> 
+    </div>   
+</body>
+</html>
