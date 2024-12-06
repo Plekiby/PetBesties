741ae0c9ad00ef5f0ee4dbc6a78c811f91f19538
Xilep05 <oscmillet@gmail.com>
2024-12-06 10:00:34 +0100

    Page Inscription
    Code de la page inscription (HTML / CSS et PHP qui est déjà dans le htdocs)

---
+++ views/Inscription.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inscription</title>
+  <link rel="stylesheet" href="/Petbesties/public/css/style_Inscription.css">
+</head>
+<body>
+  <div class="page">
+    <h1 class="title">Inscription</h1>
+    <div class="container">
+      <form class="form">
+        <input type="text" placeholder="Prénom" required>
+        <input type="text" placeholder="Nom" required>
+        <input type="number" placeholder="Âge" required>
+        <input type="text" placeholder="Code postal" required>
+        <input type="email" placeholder="Adresse mail" required>
+        <input type="password" placeholder="Créer un mot de passe" required>
+        <button type="submit">NEXT</button>
+      </form>
+      <p class="footer-text">
+        En me connectant ou en m'inscrivant, j'accepte les 
+        <a href="#">Conditions Générales de Service</a> et la 
+        <a href="#">Politique de Confidentialité</a> de PetBesties. 
+        Je consens à recevoir des e-mails et des communications marketing 
+        de la part de PetBesties et de ses affiliés et je confirme avoir 
+        18 ans ou plus.
+      </p>
+    </div>
+  </div>
+</body>
+</html>
+
